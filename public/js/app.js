5934279dfa650ee62e6c2ed43b22d7984b9b4e80
Bishop O'Connell <bishopoconnell1@gmail.com>
2017-08-15 15:32:00 -0400

    initial commit, page snap scroll implemented

---
+++ public/js/app.js
@@ -0,0 +1,19 @@
+
+$.scrollify({
+    section : ".scroll",
+    sectionName : ".scroll",
+    interstitialSection : "",
+    easing: "easeOutCubic",
+    scrollSpeed: 1000,
+    offset : 0,
+    scrollbars: false,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: true,
+    touchScroll:true,
+    before:function() {},
+    after:function() {},
+    afterResize:function() {},
+    afterRender:function() {}
+  });
