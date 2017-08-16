2539986b7352a1e3f7cb448663d054a145e3e9f4
Bishop O'Connell <bishopoconnell1@gmail.com>
2017-08-16 02:05:46 -0400

    changed background of page 2 and roughed out the first spirit description

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,16 @@
-$( document ).ready(function () {
-            $('body').panelSnap();
-            var options = {
-            $menu: $('div .menu'),
-            menuSelector: 'a',
-            panelSelector: '.drinks',
-          };
-          $('.panels').panelSnap(options);
-        });
+$(document).ready(function () {
+  var speed = {
+    slideSpeed: 500,
+    delay: 0,
+    easing: 'linear',
+  };
+  $('body').panelSnap(speed);
+  var options = {
+    $menu: $('div .menu'),
+    menuSelector: 'a',
+    panelSelector: '.drinks',
+    slideSpeed: 600,
+    easing: 'swing',
+  };
+  $('.panels').panelSnap(options);
+});
