4cf700fa9e9e0ec85f148f2b1fe5568a45354191
Bishop O'Connell <bishopoconnell1@gmail.com>
2017-08-15 20:09:24 -0400

    implemented panelSnap

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,9 @@
+$( document ).ready(function () {
+            $('body').panelSnap();
 
-$.scrollify({
-    section : ".scroll",
-    sectionName : ".scroll",
-    interstitialSection : "",
-    easing: "easeOutCubic",
-    scrollSpeed: 1000,
-    offset : 0,
-    scrollbars: false,
-    standardScrollElements: "",
-    setHeights: true,
-    overflowScroll: true,
-    updateHash: true,
-    touchScroll:true,
-    before:function() {},
-    after:function() {},
-    afterResize:function() {},
-    afterRender:function() {}
-  });
+            var options = {
+            $menu: $('div .menu')
+          };
+
+          $('.panel_container').panelSnap(options);
+        });
