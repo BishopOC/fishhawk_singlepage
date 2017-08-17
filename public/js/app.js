c5237805ba34ed9f21d7a567914babb9c709943b
Bishop O'Connell <bishopoconnell1@gmail.com>
2017-08-16 21:48:38 -0400

    added page3 as well as a nav bar

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
+
   var speed = {
+    $menu: $('.header'),
+    menuSelector: 'a',
+    panelSelector: '.snap',
     slideSpeed: 500,
     delay: 0,
-    easing: 'linear',
+    easing: 'swing',
   };
   $('body').panelSnap(speed);
   var options = {
